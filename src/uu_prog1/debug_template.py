d2717a89421e51cc788a127ee9f60541f778a721
Anders Jakobsson <ajkbssn@users.noreply.github.com>
2020-09-16 15:25:41 +0200

    feat: add timer

--- src/uu_prog1/debug_template.py
+++ src/uu_prog1/debug_template.py
@@ -1,6 +1,7 @@
 # src/uu_prog1/debug_template.py
 
 import functools
+import time
 
 
 def debug(func):
@@ -18,3 +19,17 @@ def debug(func):
 
     return wrapper_debug
 
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()  # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()  # 2
+        run_time = end_time - start_time  # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper_timer
