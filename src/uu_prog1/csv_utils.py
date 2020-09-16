35ccea77b91885e0057ea87459372b38bb23b81f
Anders Jakobsson <ajkbssn@users.noreply.github.com>
2020-09-16 19:06:11 +0200

    feat: add function to read CO2 data into dict

--- src/uu_prog1/csv_utils.py
+++ src/uu_prog1/csv_utils.py
@@ -1,17 +1,22 @@
 # src/uu_prog1/csv_utils.py
-import csv
 
+# import debug
+import csv
 
 # CO2_EMISSIONS_DATA = "./data/CO2Emissions_filtered.csv"
 
 
+# @debug
 def load_csv(filename):
     """
-    takes a string filename as argument and returns a
-    dictionary that takes the country code (all in the
-    lowercases) as the key and the list of yearly CO2
-    emission history as the value.
+    takes a string filename as argument and returns a dictionary
+    with the country code (lowercase) as key and the list of
+    yearly CO2 emission history as value.
     """
+    d = dict()
     with open(filename) as csv_file:
         reader = csv.reader(csv_file)
-    return {v[1].lower(): list(map(float, v[3:])) for v in reader[1:]}
+        for index, line in enumerate(reader):
+            if index != 0:
+                d[line[1].lower()] = list(map(float, line[3:]))
+    return d
