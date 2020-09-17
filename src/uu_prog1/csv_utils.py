31101f89470ee942d64648559b5f622fdc32ec53
Anders Jakobsson <ajkbssn@users.noreply.github.com>
2020-09-17 09:53:51 +0200

    feat: add csv reader

--- src/uu_prog1/csv_utils.py
+++ src/uu_prog1/csv_utils.py
@@ -1,17 +1,16 @@
 # src/uu_prog1/csv_utils.py
 
-# import debug
 import csv
 
-# CO2_EMISSIONS_DATA = "./data/CO2Emissions_filtered.csv"
 
-
-# @debug
 def load_csv(filename):
     """
-    takes a string filename as argument and returns a dictionary
+    Takes a string filename as argument and returns a dictionary
     with the country code (lowercase) as key and the list of
     yearly CO2 emission history as value.
+    File is CO2Emissions_filtered.csv. First row is header.
+    Fields are country_name, country_code, indicator_name and
+    then years from 1960 to 2014. Data from col 3 with first col 0.
     """
     d = dict()
     with open(filename) as csv_file:
