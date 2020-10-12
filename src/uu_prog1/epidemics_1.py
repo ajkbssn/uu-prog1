1c52fcbafe9b7e62df075ceb84e61eb03e31676d
Anders Jakobsson <ajkbssn@users.noreply.github.com>
2020-10-12 10:40:44 +0200

    fix: clean up file after move to new

--- src/uu_prog1/epidemics_1.py
+++ src/uu_prog1/epidemics_1.py
@@ -2,17 +2,16 @@
 
 import numpy as np
 
-SUSCEPTIBLE = 0
-INFECTED = 1
-RECOVERED = 2
-
 
 def SIR(S0, I0, R0, a, b, T=100):
     """
     SIR model function for back seat epidemiologists.
-    Takes initial susceptibles, infected, recovered, the rate of a susceptible becoming infected,
-    the recovery rate, and optionally how many periods the simulation shall run.
-    Returns four time series numpy arrays (susceptibles, infected, recovered, and time)
+    Takes initial susceptibles, infected, recovered,
+    the rate of a susceptible becoming infected,
+    the recovery rate, and optionally how many periods
+    the simulation shall run.
+    Returns four time series numpy arrays
+    (susceptibles, infected, recovered, and time)
     """
     S = np.zeros(T + 1, dtype="float64")
     I = np.zeros(T + 1, dtype="float64")
@@ -28,13 +27,3 @@ def SIR(S0, I0, R0, a, b, T=100):
         I[n] = I[n - 1] + a * S[n - 1] * I[n - 1] - b * I[n - 1]
         R[n] = R[n - 1] + b * I[n - 1]
     return S, I, R, t
-
-
-def createSIR2D(rows=None, columns=None):
-    pass
-    return False
-
-
-def plot2D_SIR():
-    pass
-    return False
